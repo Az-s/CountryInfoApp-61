76663a0dec2cdc844a65c75deeb25ecf7a6c9c87
Az-s <azim.sa@yahoo.com>
2021-08-19 15:24:49 +0600

    created component Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+            <Navbar bg="light">
+                <Container>
+                    <Navbar.Brand href="#home">Country Info</Navbar.Brand>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default Navbar;
